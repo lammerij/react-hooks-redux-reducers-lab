f30d1a9b7b7e75eef8d22296724e60c7086a11df
+ <+>
2022-10-27 11:59:44 -0400

    commit tree 5659c472c8e35e13418b666258ecea51955be59d

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,15 @@
-export function manageFriends(state, action) {
-  // your code here
+const initialState = { friends: [] };
+
+export function manageFriends(state = initialState, action) {
+  switch (action.type) {
+    case "friends/add":
+      state.friends.push(action.payload)
+      return { friends: state.friends};
+    case "friends/remove":
+      console.log("this is the action payload", action.payload)
+     state.friends = state.friends.filter((friend)=> friend.id !== action.payload)
+      return { friends: state.friends };
+    default:
+      return state;
+  }
 }
